063a1868f1d754c67ed43f0782ca0044fa8d88b9
Israel Silva <israeljms@gmail.com>
2023-07-08 15:38:33 -0300

    Correção de exibição dos icones no botão de download

--- src/assets/js/script/detectOS.js
+++ src/assets/js/script/detectOS.js
@@ -1,8 +1,8 @@
-const iconPath = "assets/img/icons/"
+const iconPath = "assets/img/icons/";
 const btnDownload = document.querySelector("[data-button='download']");
 var osName = "Nome S.O";
-if (navigator.appVersion.indexOf("Win")!=-1) osName="Windows";
-if (navigator.appVersion.indexOf("Mac")!=-1) osName="Apple";
-if (navigator.appVersion.indexOf("X11")!=-1) osName="UNIX";
-if (navigator.appVersion.indexOf("Linux")!=-1) osName="Linux";
-btnDownload.innerHTML += `<img src="${iconPath}icon-${osName}.svg" class="icon">Baixar para o ${osName}` 
+if (navigator.appVersion.indexOf("Win") != -1) osName = "Windows";
+if (navigator.appVersion.indexOf("Mac") != -1) osName = "Apple";
+if (navigator.appVersion.indexOf("X11") != -1) osName = "UNIX";
+if (navigator.appVersion.indexOf("Linux") != -1) osName = "Linux";
+btnDownload.innerHTML += `<img src="${iconPath}icon-${osName.toLowerCase()}.svg" class="icon">Baixar para o ${osName}`;
